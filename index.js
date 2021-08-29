f0df7f0b9d71f073783aafd8b91cf9aebcd6c830
Eric Kudler <joeento@gmail.com>
2021-08-28 22:47:56 -0400

    Adds an endpoint for uploading and has it create a source folder to upload to

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
+'use strict';
+
+const express = require('express');
+const fs = require('fs');
+const tf = require('@tensorflow/tfjs-node');
+const posenet = require('@tensorflow-models/posenet');
+
+const app = express();
+const port = 3000;
+const source = 'source/'
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+/*
+  1) Upload the image
+  2) Process the image
+    2a) Send request to lamp
+  3) Delete the image
+*/
+app.post('/upload', async (req, res) => {
+  if (!fs.existsSync(source)) {
+    fs.mkdirSync(source);
+  }
+
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
